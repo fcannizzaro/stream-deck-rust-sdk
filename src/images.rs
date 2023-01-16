bc7a5322b26a403ef0b45f160880c72d69561a70
Francesco Saverio Cannizzaro <francescosaverio.cannizzaro@gmail.com>
2023-01-16 01:13:08 +0100

    update image_to_base64 method

--- src/images.rs
+++ src/images.rs
@@ -1,3 +1,16 @@
-pub fn image_to_base64(img: Vec<u8>) -> String {
-    format!("data:image/png;base64,{}", base64::encode(img))
+use base64::{
+    alphabet,
+    engine::{self, general_purpose},
+    Engine as _,
+};
+
+pub fn image_to_base64(img: Option<Vec<u8>>) -> Option<String> {
+    if let Some(img) = img {
+        Some(format!(
+            "data:image/png;base64,{}",
+            general_purpose::STANDARD.encode(img)
+        ))
+    } else {
+        None
+    }
 }
